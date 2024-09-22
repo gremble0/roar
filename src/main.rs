e25dd7750713583121f5cea019b8b7031051b799
gremble0 <hermanstornes@gmail.com>
2024-09-22 18:53:51 +0200

    Sketch basic types for Client/Server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-use std::net::{TcpListener, TcpStream};
+use std::{
+    iter::Map,
+    net::{TcpListener, TcpStream},
+};
+
+struct Client {
+    read_buffer: Vec<u8>,
+    username: String,
+    connection: bool, // idk the type here
+}
+
+struct Server {
+    clients: Map<String, Client>,
+    listener: TcpListener,
+}
 
 fn handle_client(_stream: TcpStream) {
     println!("Incoming connection")
