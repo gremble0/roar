8ead57a03a486dff466af627c18e243d6d209dc3
gremble0 <hermanstornes@gmail.com>
2024-09-22 18:44:04 +0200

    Bind to port using TCP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(_stream: TcpStream) {
+    println!("Incoming connection")
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:1337")?;
+
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+
+    Ok(())
 }
